278e9cecb77c73f88b29a711323147da016577ad
Nora <noratian_garcia@euneiz.com>
2025-11-26 11:26:01 +0100

    creado el proyecto

---
+++ HelloWorld(NORA)/HelloWorld(NORA)/Home/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld(NORA)
+//
+//  Created by student on 26/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# HelloWorld-NORA-
-Ejercicio HelloWorld
