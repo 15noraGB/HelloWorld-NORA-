7dabd7903bf8e58352e1220a0848f65ec552d991
Nora <noratian_garcia@euneiz.com>
2025-12-03 12:54:26 +0100

    listaCompra_2 completo

--- HelloWorld(NORA)/HelloWorld(NORA)/ListaCompra/ListaCompraViewController.swift
+++ HelloWorld(NORA)/HelloWorld(NORA)/ListaCompra/ListaCompraViewController.swift
@@ -12,39 +12,43 @@ class ListaCompraViewController: UIViewController {
     
     @IBOutlet weak var listaView: UITableView!
     
-    // 1. Array de elementos de la compra
     let shopItems = ["Leche", "Pan", "Huevos", "Fruta", "Verduras"]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
         
-        // Conectar la tabla con el DataSource
-        listaView.dataSource = self
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Conectar la tabla con el DataSource y Delegate
+            listaView.dataSource = self
+            listaView.delegate = self
+        }
     }
-}
 
-// 2. Extensión para implementar UITableViewDataSource
-extension ListaCompraViewController: UITableViewDataSource {
+// Extensión para UITableViewDataSource y UITableViewDelegate
+extension ListaCompraViewController: UITableViewDataSource, UITableViewDelegate {
     
-    // Número de secciones
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
-    // Número de filas por sección
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shopItems.count
     }
     
-    // Configuración de la celda
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // El identificador de la celda debe coincidir con el definido en el Storyboard
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = shopItems[indexPath.row]
         return cell
     }
+    
+    // Detectar selección de fila
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let itemSeleccionado = shopItems[indexPath.row]
+        print("Elemento seleccionado: \(itemSeleccionado)")
+        
+        // Deseleccionamos la fila para que no quede marcada
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
-
     /*
     // MARK: - Navigation
 
