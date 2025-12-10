112e5e16f78a49b8873ad65e764f24e41328dc9e
Nora <noratian_garcia@euneiz.com>
2025-12-10 10:11:08 +0100

    segue detalle ListaCompra funciona

--- HelloWorld(NORA)/HelloWorld(NORA)/ListaCompra/DetalleListaViewController.swift
+++ HelloWorld(NORA)/HelloWorld(NORA)/ListaCompra/DetalleListaViewController.swift
@@ -1,32 +1,15 @@
-//
-//  DetalleListaViewController.swift
-//  HelloWorld(NORA)
-//
-//  Created by student on 3/12/25.
-//
-
 import UIKit
 
 class DetalleListaViewController: UIViewController {
 
     @IBOutlet weak var producto: UILabel!
     
+    // Variable donde llegará el producto seleccionado
+    var nombreProducto: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        producto.text = nombreProducto
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- HelloWorld(NORA)/HelloWorld(NORA)/ListaCompra/ListaCompraViewController.swift
+++ HelloWorld(NORA)/HelloWorld(NORA)/ListaCompra/ListaCompraViewController.swift
@@ -1,62 +1,54 @@
-//
-//  ListaCompraViewController.swift
-//  HelloWorld(NORA)
-//
-//  Created by student on 3/12/25.
-//
-
 import UIKit
 
 class ListaCompraViewController: UIViewController {
-    
-    
+
     @IBOutlet weak var listaView: UITableView!
     
     let shopItems = ["Leche", "Pan", "Huevos", "Fruta", "Verduras"]
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            
-            // Conectar la tabla con el DataSource y Delegate
-            listaView.dataSource = self
-            listaView.delegate = self
-        }
-    }
-
-// Extensión para UITableViewDataSource y UITableViewDelegate
-extension ListaCompraViewController: UITableViewDataSource, UITableViewDelegate {
     
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+    // Variable para guardar qué item se seleccionó
+    var itemSeleccionado: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        listaView.dataSource = self
+        listaView.delegate = self
     }
+}
+
+// MARK: - Tabla
+extension ListaCompraViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return shopItems.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = shopItems[indexPath.row]
         return cell
     }
     
-    // Detectar selección de fila
+    // Cuando se toca una fila
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let itemSeleccionado = shopItems[indexPath.row]
-        print("Elemento seleccionado: \(itemSeleccionado)")
+
+        itemSeleccionado = shopItems[indexPath.row]
         
-        // Deseleccionamos la fila para que no quede marcada
-        tableView.deselectRow(at: indexPath, animated: true)
+        // Llamamos al segue
+        performSegue(withIdentifier: "detalleSegue", sender: self)
     }
 }
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+// MARK: - Segue → enviar dato al detalle
+extension ListaCompraViewController {
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "detalleSegue" {
+            let destino = segue.destination as! DetalleListaViewController
+            destino.nombreProducto = itemSeleccionado
+        }
     }
-    */
-
-
+}
